c1eecda3adb1f4a04e2d04e7a89a6b388490081a
Marcin Pojmaj <marcinpojmaj1@gmail.com>
2022-11-06 17:19:43 +0100

    Rename main.py to langanki.py

--- langanki.py
+++ langanki.py
@@ -3,7 +3,9 @@ import argparse
 from libs.exporters.anki import AnkiExporter
 from libs.importers import get_importer, ImporterName
 
+
 if __name__ == '__main__':
+
     parser = argparse.ArgumentParser(description='Welcome to langanki!')
     parser.add_argument("-i", "--importer", choices=['google'], required=True, type=str, help="Importer for Cards")
 
